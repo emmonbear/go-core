c4613563555aba24a91bef8cd2989ef88587386a
emmonbea <moskaleviluak@icloud.com>
2024-12-13 23:13:48 +0500

    feat(ex3): add documentation

---
+++ 3/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type StringIntMap struct {
+	data map[string]int
+}
+
+func NewStringIntMap() *StringIntMap {
+	return &StringIntMap{
+		data: make(map[string]int),
+	}
+}
+
+func (m *StringIntMap) Add(key string, value int) {
+	m.data[key] = value
+}
+
+func main() {
+	m := NewStringIntMap()
+	m.Add("string 1", 1)
+
+	fmt.Println(m)
+}
