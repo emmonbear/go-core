6e0164c7228ca825642737ccea32c2451e5c0e0f
emmonbea <moskaleviluak@icloud.com>
2024-12-14 15:41:06 +0500

    feat(ex3): add a method that deletes an item by key

--- 3/main.go
+++ 3/main.go
@@ -16,9 +16,15 @@ func (m *StringIntMap) Add(key string, value int) {
 	m.data[key] = value
 }
 
+func (m *StringIntMap) Remove(key string) {
+	delete(m.data, key)
+}
+
 func main() {
 	m := NewStringIntMap()
 	m.Add("string 1", 1)
+	m.Add("string 2", 2)
+	m.Remove("string 1")
 
 	fmt.Println(m)
 }
