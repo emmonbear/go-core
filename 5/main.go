848091331977ff26aff383442c3b4e540da52b5d
emmonbea <moskaleviluak@icloud.com>
2024-12-14 19:13:30 +0500

    docs(ex5): add documentation

--- 5/main.go
+++ 5/main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+// Compare function compares two slices of integers and finds the common elements.
+// It returns a boolean indicating whether there is at least one common element,
+// and a slice containing the unique common elements.
+//
+// Parameters:
+// - slice1 []int: The first slice of integers to compare.
+// - slice2 []int: The second slice of integers to compare.
+//
+// Returns:
+// - bool: true if there are any common elements, false otherwise.
+// - []int: A slice containing the unique common elements between slice1 and slice2.
 func Compare(slice1, slice2 []int) (bool, []int) {
 	set2 := make(map[int]struct{})
 	for _, v := range slice2 {
