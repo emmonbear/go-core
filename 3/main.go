da2ee4f6221ffdbea0aaeadedad8b658d57e0310
emmonbea <moskaleviluak@icloud.com>
2024-12-14 17:08:02 +0500

    feat(ex3): add a method that returns a new map containing all elements of the current map

--- 3/main.go
+++ 3/main.go
@@ -20,11 +20,30 @@ func (m *StringIntMap) Remove(key string) {
 	delete(m.data, key)
 }
 
+func (m *StringIntMap) Copy() map[string]int {
+	if m.data == nil {
+		return nil
+	}
+	result := make(map[string]int, len(m.data))
+
+	for key, value :=  range m.data {
+		result[key] = value
+	}
+
+	return result
+}
+
 func main() {
 	m := NewStringIntMap()
 	m.Add("string 1", 1)
 	m.Add("string 2", 2)
+	m.Add("string 3", 3)
 	m.Remove("string 1")
 
 	fmt.Println(m)
+	
+	copy := m.Copy()
+	m.Remove("string 3")
+	fmt.Println(m)
+	fmt.Println(copy)
 }
