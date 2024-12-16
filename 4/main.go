12b80a90bbdbc1ff260baa2e2a623212527ce322
emmonbea <moskaleviluak@icloud.com>
2024-12-16 17:31:23 +0500

    style: fix style

--- 4/main.go
+++ 4/main.go
@@ -5,15 +5,17 @@ import "fmt"
 // Difference takes two slices of strings and returns a slice of strings
 // containing the elements that are in slice1 but not in slice2.
 //
-// The function uses a map (set2) to store the elements of slice2 for 
+// The function uses a map (set2) to store the elements of slice2 for
 // efficient lookup while iterating over slice1.
 //
 // Parameters:
-//   slice1: A slice of strings, from which the difference will be calculated.
-//   slice2: A slice of strings, the elements of which will be excluded from slice1.
+//
+//	slice1: A slice of strings, from which the difference will be calculated.
+//	slice2: A slice of strings, the elements of which will be excluded from slice1.
 //
 // Returns:
-//   A slice of strings that contains the elements from slice1 that are not in slice2.
+//
+//	A slice of strings that contains the elements from slice1 that are not in slice2.
 func Diffence(slice1, slice2 []string) []string {
 	set2 := make(map[string]struct{})
 	for _, v := range slice2 {
@@ -32,7 +34,7 @@ func Diffence(slice1, slice2 []string) []string {
 }
 
 func main() {
-	slice1 := []string{"apple", "banana", "cherry", "date", "43", "lead", "gno1"} 
+	slice1 := []string{"apple", "banana", "cherry", "date", "43", "lead", "gno1"}
 	slice2 := []string{"banana", "date", "fig"}
 
 	set := Diffence(slice1, slice2)
