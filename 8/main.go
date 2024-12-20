ef4d3755ac0aeec19cc0f751e8a3f93a514a4205
emmonbea <moskaleviluak@icloud.com>
2024-12-20 08:51:25 +0500

    fix(ex8): change the use of mutex to atomics

--- 8/main.go
+++ 8/main.go
@@ -2,52 +2,45 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"sync/atomic"
 	"time"
 )
 
 type CustomWaitGroup struct {
-	semaphore chan struct{}
-	counter   int
-	mutex sync.Mutex
+	ch    chan struct{}
+	count int32
 }
 
 func NewCustomWaitGroup() *CustomWaitGroup {
-	return &CustomWaitGroup{
-		semaphore: make(chan struct{}),
+	return &CustomWaitGroup{ch: make(chan struct{})}
+}
+
+func (cwg *CustomWaitGroup) Add(delta int32) {
+	atomic.AddInt32(&cwg.count, delta)
+	if atomic.LoadInt32(&cwg.count) == 0 {
+		close(cwg.ch)
 	}
 }
 
-func (wg *CustomWaitGroup) Add(delta int) {
-	wg.mutex.Lock()
-	defer wg.mutex.Unlock()
-	wg.counter += delta
-	if wg.counter == 0 {
-		close(wg.semaphore)
-	}
+func (cwg *CustomWaitGroup) Done() {
+	cwg.Add(-1)
 }
 
-func (wg *CustomWaitGroup) Done() {
-	wg.Add(-1)
-}
-
-func (wg *CustomWaitGroup) Wait() {
-	<-wg.semaphore
+func (cwg *CustomWaitGroup) Wait() {
+	<-cwg.ch
 }
 
 func main() {
-	wg := NewCustomWaitGroup()
-
+	cwg := NewCustomWaitGroup()
 	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		i := i
-		go func(id int) {
-			defer wg.Done()
-			fmt.Printf("Worker %d starting\n", id)
+		cwg.Add(1)
+		go func(index int) {
+			defer cwg.Done()
+			fmt.Printf("worker %d starting\n", i)
 			time.Sleep(time.Second)
-			fmt.Printf("Worker %d done\n", id)
+			fmt.Printf("worker %d done\n", i)
 		}(i)
 	}
-	wg.Wait()
+	cwg.Wait()
 	fmt.Println("All workers done")
 }
